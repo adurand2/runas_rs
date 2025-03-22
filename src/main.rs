123696faddac9050431a22c2352db25a0e3dd911
adurand2 <56481533+adurand2@users.noreply.github.com>
2025-03-22 15:11:22 -0400

    reformatted to run in multiple sessions
    sessions 1-4

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use windows::Win32::Security::{DuplicateTokenEx, SecurityImpersonation, TOKEN_AC
 use windows::Win32::Foundation::GetLastError;
 use windows::Win32::Foundation::HANDLE;
 use windows::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_OK};
+use windows::Win32::System::RemoteDesktop::WTSGetActiveConsoleSessionId;
 use std::env;
 
 fn str_to_pwstr(input: &str) -> *mut u16 {
@@ -26,6 +27,79 @@ fn str_to_pwstr(input: &str) -> *mut u16 {
     ptr
 }
 
+fn runProcess(lpcommandline: PWSTR, session_id: u32 ){
+
+    unsafe {
+
+        // Process creation flags are set to 0
+        let dwcreationflags = PROCESS_CREATION_FLAGS(0);
+
+        /*
+            MessageBoxW(
+                None,
+                lpcommandline,           // Message
+                lpcommandline,           // Title
+                MB_OK,           // OK button
+            );
+        */
+
+   
+           // create handle pointers for the original user token and the duplicated one
+           let mut old_handle: HANDLE = Default::default();
+           let old_token : *mut HANDLE = &mut old_handle; 
+   
+           let mut new_handle: HANDLE = Default::default();
+           let new_token : *mut HANDLE = &mut new_handle;
+   
+   
+           // find the session ID of current user
+           //let session_id = WTSGetActiveConsoleSessionId();
+           println!("running in session {}", session_id);
+   
+   
+           // Try to retrieve the user token for the specified session
+           let qut_result = WTSQueryUserToken(session_id, old_token);
+           if let Err(e) = qut_result {
+   
+               println!("Error getting user token: {}",e);
+               println!("{}", GetLastError().to_hresult());
+               return;
+           }
+           
+           // Duplicate the token
+           let dte_result = DuplicateTokenEx(*old_token,TOKEN_ACCESS_MASK(0), None, SecurityImpersonation, TokenPrimary, new_token);
+           if let Err(e) = dte_result {
+               println!("Error duplicating token: {}",e);
+               return;
+           }
+   
+           // Output var for process information
+           let mut lpprocessinformation: PROCESS_INFORMATION = Default::default();
+           let lpprocessinformation_ptr : *mut PROCESS_INFORMATION = &mut lpprocessinformation; 
+   
+           // Startup options to ensure that the program renders on the GUI
+           let mut lpstartupinfo = STARTUPINFOW{
+               lpDesktop: PWSTR(str_to_pwstr("winsta0\\default")), // The default GUI desktop
+               dwFlags: STARTF_USESHOWWINDOW,
+               wShowWindow: 1,
+               ..Default::default()
+           };
+   
+           lpstartupinfo.cb = std::mem::size_of_val(&lpstartupinfo) as u32;
+           
+           // finally, create the process as the other user
+           let create_process_result = CreateProcessAsUserW(Some(*new_token), None , Some(lpcommandline) , None, None, false, dwcreationflags, None, None, &lpstartupinfo, lpprocessinformation_ptr);
+           if let Err(e) = create_process_result {
+               println!("Error creating process: {}",e);
+               println!("{}", GetLastError().to_hresult());
+               return;
+           }
+   
+           println!("congrats on your new process!");
+           return;
+       }
+}
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
@@ -39,77 +113,33 @@ fn main() {
     let single_string: String = args.iter().skip(1).cloned().collect::<Vec<String>>().join(" ");
 
     let fullarglist = format!("\"{}\"", single_string);
-
     //println!("{}", fullarglist);
 
+
     unsafe{
-
-        // Process creation flags are set to 0
-        let dwcreationflags = PROCESS_CREATION_FLAGS(0);
         
-
+        // Hardcode sessionID to 1 for now
+        let session_id = 0;
+        
         // Convert executable string to PWSTR
         let lpcommandline = PWSTR(str_to_pwstr(&fullarglist));
 
-    /*
-        MessageBoxW(
-            None,
-            lpcommandline,           // Message
-            lpcommandline,           // Title
-            MB_OK,           // OK button
-     );
-    */
-        // Hardcode sessionID to 1 for now
+        // run on a ton of session ids
+        runProcess(lpcommandline, session_id);
+
+        let session_id = 1;
+        runProcess(lpcommandline, session_id);
+
+        let session_id = 2;
+        runProcess(lpcommandline, session_id);
+
         let session_id = 3;
+        runProcess(lpcommandline, session_id);
 
-        // create handle pointers for the original user token and the duplicated one
-        let mut old_handle: HANDLE = Default::default();
-        let old_token : *mut HANDLE = &mut old_handle; 
+        let session_id = 4; 
+        runProcess(lpcommandline, session_id);
 
-        let mut new_handle: HANDLE = Default::default();
-        let new_token : *mut HANDLE = &mut new_handle;
-
-
-
-        // Try to retrieve the user token for the specified session
-        let qut_result = WTSQueryUserToken(session_id, old_token);
-        if let Err(e) = qut_result {
-
-            println!("Error getting user token: {}",e);
-            println!("{}", GetLastError().to_hresult());
-            return;
-        }
         
-        // Duplicate the token
-        let dte_result = DuplicateTokenEx(*old_token,TOKEN_ACCESS_MASK(0), None, SecurityImpersonation, TokenPrimary, new_token);
-        if let Err(e) = dte_result {
-            println!("Error duplicating token: {}",e);
-            return;
-        }
-
-        // Output var for process information
-        let mut lpprocessinformation: PROCESS_INFORMATION = Default::default();
-        let lpprocessinformation_ptr : *mut PROCESS_INFORMATION = &mut lpprocessinformation; 
-
-        // Startup options to ensure that the program renders on the GUI
-        let mut lpstartupinfo = STARTUPINFOW{
-            lpDesktop: PWSTR(str_to_pwstr("winsta0\\default")), // The default GUI desktop
-            dwFlags: STARTF_USESHOWWINDOW,
-            wShowWindow: 1,
-            ..Default::default()
-        };
-
-        lpstartupinfo.cb = std::mem::size_of_val(&lpstartupinfo) as u32;
         
-        // finally, create the process as the other user
-        let create_process_result = CreateProcessAsUserW(Some(*new_token), None , Some(lpcommandline) , None, None, false, dwcreationflags, None, None, &lpstartupinfo, lpprocessinformation_ptr);
-        if let Err(e) = create_process_result {
-            println!("Error creating process: {}",e);
-            println!("{}", GetLastError().to_hresult());
-            return;
-        }
-
-        println!("congrats on your new process!");
-        return;
     }
 }
